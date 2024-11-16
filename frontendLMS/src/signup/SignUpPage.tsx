7790d2cc9a49ef510f3e854f716e00b3df80121f
Sandip Rajbhandari <srb.newar.yb0111@gmail.com>
2024-11-16 14:42:47 +0545

    init tailwind css

---
+++ frontendLMS/src/signup/SignUpPage.tsx
@@ -0,0 +1,13 @@
+
+
+const SignUpPage = () => {
+    return (
+        <>
+            <div className="h-screen flex justify-center items-center bg-gray-100">
+                <h1 className="text-4xl font-bold text-blue-500">Sign Up</h1>
+            </div>
+        </>
+    )
+}
+
+export default SignUpPage
